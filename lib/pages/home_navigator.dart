5bcd64a3d87c54075e4d6f9ebe11143c797a00cb
P.K Selani Didualni <pkselani00@gmail.com>
2024-01-13 17:09:28 -0800

    bottom nav bar updated

--- lib/pages/home_navigator.dart
+++ lib/pages/home_navigator.dart
@@ -1,6 +1,6 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
-import 'package:travel_journal/components/bottom_navigation_bar.dart';
+import 'package:travel_journal/components/app_colors.dart';
 import 'package:travel_journal/models/firebase_user_model.dart';
 import 'package:travel_journal/pages/Journey/journey_add_page.dart';
 import 'package:travel_journal/pages/Map/map_page.dart';
@@ -17,35 +17,7 @@ class HomeNavigator extends StatefulWidget {
 }
 
 class _HomeNavigatorState extends State<HomeNavigator> {
-  Menus currentIndex = Menus.home;
-  AuthService authService = AuthService();
-  FirebaseFirestore _firestore = FirebaseFirestore.instance;
-  late Future<UserWithCredentials?> user;
-
-  //getcurrent user detils
-
-  @override
-  void initState() {
-    super.initState();
-    
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: pages[currentIndex.index],
-      bottomNavigationBar: MyBottonNavigation(
-        currentIndex: currentIndex,
-        onTap: ((value) {
-          setState(() {
-            currentIndex = value;
-            print(currentIndex);
-          });
-        }),
-      ),
-    );
-  }
-
+  int index = 0;
   final pages = [
     NoteHomePage(),
     WeatherPage(),
@@ -53,4 +25,73 @@ class _HomeNavigatorState extends State<HomeNavigator> {
     MapPage(),
     ProfilePage(),
   ];
+
+  // Menus currentIndex = Menus.home;
+  // AuthService authService = AuthService();
+  late Future<UserWithCredentials?> user;
+
+  //getcurrent user detils
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Items = <Widget>[
+      Icon(
+        Icons.home,
+        size: 30,
+      ),
+      Icon(
+        Icons.cloud,
+        size: 30,
+      ),
+      Icon(
+        Icons.add,
+        size: 30,
+      ),
+      Icon(
+        Icons.map,
+        size: 30,
+      ),
+      Icon(
+        Icons.person,
+        size: 30,
+      )
+    ];
+
+    return SafeArea(
+      child: Scaffold(
+        extendBody: true,
+        body: pages[index],
+        bottomNavigationBar: Theme(
+          data: Theme.of(context).copyWith(
+            iconTheme: IconThemeData(
+              color: Colors.white, //this will change the icon color
+            ),
+          ),
+          child: CurvedNavigationBar(
+            color: AppColors.mainColor, //Navigation bar Color change
+            buttonBackgroundColor:
+                AppColors.mainColor, //change the selected background color
+            backgroundColor: Colors.transparent,
+            index: index,
+            animationCurve:
+                Curves.easeInOut, //Thransition duration change in here
+            animationDuration: Duration(
+                milliseconds: 500), //Thransition duration change in here
+            items: Items,
+            height: 60,
+            onTap: (index) => setState(() {
+              this.index = index;
+            }),
+          ),
+        ),
+
+        
+      ),
+    );
+  }
 }
